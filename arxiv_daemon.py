0f64e0b66fa4b159a6f721dc291bd922128927f3
Yehor Smoliakov <yehors@ukr.net>
2022-05-21 12:57:10 +0300

    Add Computer Science query string

--- arxiv_daemon.py
+++ arxiv_daemon.py
@@ -13,6 +13,50 @@ import argparse
 from aslite.arxiv import get_response, parse_response
 from aslite.db import get_papers_db, get_metas_db
 
+CS = '''cs.AI - Artificial Intelligence
+cs.CL - Computation and Language
+cs.CC - Computational Complexity
+cs.CE - Computational Engineering, Finance, and Science
+cs.CG - Computational Geometry
+cs.GT - Computer Science and Game Theory
+cs.CV - Computer Vision and Pattern Recognition
+cs.CY - Computers and Society
+cs.CR - Cryptography and Security
+cs.DS - Data Structures and Algorithms
+cs.DB - Databases
+cs.DL - Digital Libraries
+cs.DM - Discrete Mathematics
+cs.DC - Distributed, Parallel, and Cluster Computing
+cs.ET - Emerging Technologies
+cs.FL - Formal Languages and Automata Theory
+cs.GL - General Literature
+cs.GR - Graphics
+cs.AR - Hardware Architecture
+cs.HC - Human-Computer Interaction
+cs.IR - Information Retrieval
+cs.IT - Information Theory
+cs.LO - Logic in Computer Science
+cs.LG - Machine Learning
+cs.MS - Mathematical Software
+cs.MA - Multiagent Systems
+cs.MM - Multimedia
+cs.NI - Networking and Internet Architecture
+cs.NE - Neural and Evolutionary Computing
+cs.NA - Numerical Analysis
+cs.OS - Operating Systems
+cs.OH - Other Computer Science
+cs.PF - Performance
+cs.PL - Programming Languages
+cs.RO - Robotics
+cs.SI - Social and Information Networks
+cs.SE - Software Engineering
+cs.SD - Sound
+cs.SC - Symbolic Computation
+cs.SY - Systems and Control'''.splitlines()
+CS = [x.split(' - ')[0] for x in CS]
+CS = ['cat:' + x + '+OR+' for x in CS]
+CS = ''.join(CS)
+
 if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO, format='%(name)s %(levelname)s %(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
